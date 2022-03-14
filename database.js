4e1af010e8f785c550094ead861b1aba717ecfbc
Nacretion <49387460+Nacretion@users.noreply.github.com>
2022-03-14 18:34:27 +0500

    heroku database

--- database.js
+++ database.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-    user: "postgres",
-    password: "",
-    host: "localhost",
+    user: "hnfbdhrrbowwxm",
+    password: "b753b63ee9bd56f9268c1544a63185704585461684d9844cce1544910a903cc7",
+    host: "ec2-52-208-185-143.eu-west-1.compute.amazonaws.com",
     port: "5432",
-    database: "WelbeX"
+    database: "d6a8iter1ebnsk"
 })
 
 module.exports = pool
